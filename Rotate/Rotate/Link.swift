749c131aea080cdcf77ed9b7b823fe079d423f5e
iLoser <85982449+iLoser@users.noreply.github.com>
2021-08-26 16:38:33 +0530

    Add files via upload

---
+++ Rotate/Rotate/Link.swift
@@ -0,0 +1,13 @@
+//
+//  Link.swift
+//  Rotate
+//
+//  Created by Greens-Apple2 on 26/08/21.
+//  Copyright © 2021 Greens-Apple2. All rights reserved.
+//
+
+import Foundation
+
+struct MyVariables {
+    static var link = "https://reqres.in/api/users?delay=3"
+}
